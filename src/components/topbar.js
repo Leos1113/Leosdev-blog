6772516b617b53c3a5d70b0ab4922dbd889ec362
Leos1113 <edgarleonmartin13@gmail.com>
2020-08-07 20:53:33 +0200

    Fix topbar

--- src/components/topbar.js
+++ src/components/topbar.js
@@ -1,22 +1,33 @@
 import React from "react"
 import { Link } from "gatsby"
+import { Match } from "@reach/router"
 
 import Logo from '../../content/assets/leosdev-logo.png';
+import HomeIcon from '../../content/assets/home-icon.png';
 
 
 const Topbar = () => {
     return(
       <nav className="flex items-center justify-between flex-wrap bg-black">
-        <div className="flex items-center flex-shrink-0 text-white bg-black mr-6 ml-6">
-          <Link
-            style={{
-              boxShadow: `none`,
-            }}
-            to={`/`}
-          >
-            <img src={Logo} className="logo-img" alt="Logo de Leos Dev"></img>
-          </Link>
-        </div>
+          <p></p>
+          <img src={Logo} className="logo-img" alt="Logo de Leos Dev"></img>
+          <Match path="/">
+              {props =>
+                  props.match ? (
+                      <div></div>
+                  ) : (
+                      <Link
+
+                          style={{
+                              boxShadow: "none",
+                          }}
+                          to={"/"}
+                      >
+                          <img src={HomeIcon} className="home-icon" alt="Icono de la home"/>
+                      </Link>
+                  )
+              }
+          </Match>
       </nav>
     )
 }
